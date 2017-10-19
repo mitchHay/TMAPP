4003307b75682d20cd1c3914d20a376c581f43c1
mitchHay <30483032+mitchHay@users.noreply.github.com>
2017-10-19 14:11:36 +1000

    ENABLED FULL ADDRESS V2.0

--- app/src/main/java/com/bignerdranch/android/timemanagement/LocatrActivity.java
+++ app/src/main/java/com/bignerdranch/android/timemanagement/LocatrActivity.java
@@ -1,18 +1,34 @@
 package com.bignerdranch.android.timemanagement;
 
 import android.app.Dialog;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by mitchellhayward on 9/10/17.
  */
 
-public class LocatrActivity extends SingleFragmentActivity {
+public class LocatrActivity extends SingleFragmentActivity implements OnMapReadyCallback {
 
     private static final int REQUEST_ERROR = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.locatr_fragment);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
     @Override
     protected Fragment createFragment(){
@@ -31,4 +47,8 @@ public class LocatrActivity extends SingleFragmentActivity {
         }
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
